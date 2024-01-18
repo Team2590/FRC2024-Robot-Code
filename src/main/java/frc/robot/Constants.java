545c498bbc015d1dade60295be0943f7b3fe2d94
Arnav Nayak <arnavnayak@icloud.com>
2024-01-17 20:31:54 -0500

    tunable paremeters for the shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,7 +23,7 @@ package frc.robot;
  */
 public final class Constants {
   public static final Mode currentMode = Mode.REAL;
-
+  public static final boolean tuningMode = true;
   public static enum Mode {
     /** Running on a real robot. */
     REAL,
--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -40,6 +40,9 @@ public class Shooter extends SubsystemBase {
     // separate robot with different tuning)
     switch (Constants.currentMode) {
       case REAL:
+        ffModel = new SimpleMotorFeedforward(0.1,0.5);
+        io.configurePID(1,0, 0);
+        break;
       case REPLAY:
         ffModel = new SimpleMotorFeedforward(0.1, 0.05);
         io.configurePID(1.0, 0.0, 0.0);
