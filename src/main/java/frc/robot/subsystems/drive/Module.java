89dc3f3c6b71b53d18d701b824e38fa41d7a87b2
peter-wolfe <wolfe@pobox.com>
2024-01-21 17:39:51 -0500

    Updated PID values

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -40,12 +40,12 @@ public class Module {
   private double lastPositionMeters = 0.0; // Used for delta calculation
   private SwerveModulePosition[] positionDeltas = new SwerveModulePosition[] {};
 
-  LoggedTunableNumber wheelRadius = new LoggedTunableNumber("Drive/Module/WheelRadius", 0);
-  LoggedTunableNumber driveKp = new LoggedTunableNumber("Drive/Module/DriveKp", 0.0);
+  LoggedTunableNumber wheelRadius = new LoggedTunableNumber("Drive/Module/WheelRadius", 2);
+  LoggedTunableNumber driveKp = new LoggedTunableNumber("Drive/Module/DriveKp", 0.1);
   LoggedTunableNumber driveKd = new LoggedTunableNumber("Drive/Module/DriveKd", 0);
   LoggedTunableNumber driveKs = new LoggedTunableNumber("Drive/Module/DriveKs", 0.19578);
   LoggedTunableNumber driveKv = new LoggedTunableNumber("Drive/Module/DriveKv", 0.11483);
-  LoggedTunableNumber turnKp = new LoggedTunableNumber("Drive/Module/TurnKp", 0);
+  LoggedTunableNumber turnKp = new LoggedTunableNumber("Drive/Module/TurnKp", 7);
   LoggedTunableNumber turnKd = new LoggedTunableNumber("Drive/Module/TurnKd", 0);
 
   SimpleMotorFeedforward driveFeedforward;
@@ -61,8 +61,8 @@ public class Module {
     switch (Constants.currentMode) {
       case REAL:
         driveFeedforward = new SimpleMotorFeedforward(0.19578, 0.11483);
-        driveFeedback = new PIDController(0.0, 0.0, 0.0, 0.02);
-        turnFeedback = new PIDController(0, 0.0, 0.0, 0.02);
+        driveFeedback = new PIDController(0.1, 0.0, 0.0, 0.02);
+        turnFeedback = new PIDController(7, 0.0, 0.0, 0.02);
       case REPLAY:
         driveFeedforward = new SimpleMotorFeedforward(0.19578, 0.11483);
         driveFeedback = new PIDController(0.05, 0.0, 0.0);
