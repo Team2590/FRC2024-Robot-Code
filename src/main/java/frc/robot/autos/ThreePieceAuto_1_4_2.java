90d671419aa36a0e2b67140567b8655a8efb7a00
asimfazal <fazal.asim@gmail.com>
2024-02-17 15:35:45 -0500

    Added print messages

--- src/main/java/frc/robot/autos/ThreePieceAuto_1_4_2.java
+++ src/main/java/frc/robot/autos/ThreePieceAuto_1_4_2.java
@@ -1,5 +1,6 @@
 package frc.robot.autos;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Superstructure;
@@ -12,14 +13,20 @@ public class ThreePieceAuto_1_4_2 extends SequentialCommandGroup {
     addRequirements(drive);
 
     addCommands(
-        // TODO: command to start the start the intake system.
-        // Move to note1 from starting position B (speaker)
-        paths.getFollowPathCommand("startB_note1"),
+       // Start up the intake system and follow path to first position in parallel.
+        Commands.parallel(
+          // TODO: command to start the start the intake system.
+          Commands.print("Starting up Intake .... "),
+          // Move to note1 from starting position B (speaker)
+          paths.getFollowPathCommand("startB_note1")
+        ),
+        Commands.print("Picking up Note1 "),
         // Intake
         new InstantCommand(() -> superstructure.intake()),
         // TODO: Position the robote towards speaker??
         // Could we use snapping logic to lock on to target.
         // Shoot
+        Commands.print("Shoot Note1"),
         new InstantCommand(() -> superstructure.shoot()));
   }
 }
