f9ae215b8de35408d1ce877b34f27d2df746b949
asimfazal <fazal.asim@gmail.com>
2024-02-18 14:09:49 -0500

    Added some comments

--- src/main/java/frc/robot/autos/PathPlannerPaths.java
+++ src/main/java/frc/robot/autos/PathPlannerPaths.java
@@ -7,7 +7,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj2.command.Command;es. */
+import edu.wpi.first.wpilibj2.command.Command;
 public class PathPlannerPaths {
 
   private final Map<String, PathPlannerPath> paths;
