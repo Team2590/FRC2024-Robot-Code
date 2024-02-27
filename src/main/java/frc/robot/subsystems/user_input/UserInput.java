28b0e94340075bc23f3f6f3eeee51842cb4f6995
Ian Keller <73369656+IanK524@users.noreply.github.com>
2024-02-26 19:48:35 -0500

    User Input Rewrite (#30)
    * controller support w/ all methods
    
    * fixed some stuff

--- src/main/java/frc/robot/subsystems/user_input/UserInput.java
+++ src/main/java/frc/robot/subsystems/user_input/UserInput.java
@@ -1,22 +1,21 @@
 package frc.robot.subsystems.user_input;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.util.BandedJoystick;
-import frc.robot.util.NemesisSubsystem;
 import frc.robot.util.Smoother;
 
 /**
- * Handles input from 2 joysticks and a button panel. The button panel is numbered 1 to 18, numbered
- * as if reading English.
+ * Handles input from 2 joysticks and an xbox controller.
  *
  * @author Elan Ronen
+ * @author Ian Keller
  * @see <a
  *     href="https://www.digitalcombatsimulator.com/upload/iblock/3c2/Template-T16000m-Hotas.jpg">Joystick
- *     Button Map</a>\
+ *     Button Map</a>
  */
-public class UserInput extends NemesisSubsystem implements RobotMap {
+public class UserInput extends SubsystemBase implements RobotMap {
 
   private static UserInput instance;
 
@@ -29,7 +28,6 @@ public class UserInput extends NemesisSubsystem implements RobotMap {
   private final Smoother.Wrapper leftXInput;
   private final Smoother.Wrapper leftYInput;
   private final Smoother.Wrapper rightXInput;
-  private final Joystick operatorPanel;
   private final XboxController operatorController;
 
   private UserInput() {
@@ -39,7 +37,6 @@ public class UserInput extends NemesisSubsystem implements RobotMap {
     leftXInput = Smoother.wrap(2, leftJoystick::getXBanded);
     leftYInput = Smoother.wrap(2, leftJoystick::getYBanded);
     rightXInput = Smoother.wrap(1, rightJoystick::getXBanded);
-    operatorPanel = new Joystick(3);
     operatorController = new XboxController(2);
   }
 
@@ -67,6 +64,10 @@ public class UserInput extends NemesisSubsystem implements RobotMap {
     return leftJoystick.getRawButtonPressed(button);
   }
 
+  public boolean leftJoystickButtonReleased(int button) {
+    return leftJoystick.getRawButtonReleased(button);
+  }
+
   public int leftJoystickPOV() {
     return leftJoystick.getPOV();
   }
@@ -77,6 +78,10 @@ public class UserInput extends NemesisSubsystem implements RobotMap {
 
   public boolean leftJoystickTriggerPressed() {
     return leftJoystick.getTriggerPressed();
+  }
+
+  public boolean leftJoystickTriggerReleased() {
+    return leftJoystick.getTriggerReleased();
   }
 
   // RIGHT JOYSTICK
@@ -93,6 +98,10 @@ public class UserInput extends NemesisSubsystem implements RobotMap {
     return rightJoystick.getRawButtonPressed(button);
   }
 
+  public boolean rightJoystickButtonReleased(int button) {
+    return rightJoystick.getRawButtonReleased(button);
+  }
+
   public int rightJoystickPOV() {
     return rightJoystick.getPOV();
   }
@@ -105,50 +114,109 @@ public class UserInput extends NemesisSubsystem implements RobotMap {
     return rightJoystick.getTriggerPressed();
   }
 
-  // OPERATOR PANEL
-
-  public boolean operatorPanelButton(int button) {
-    return operatorPanel.getRawButton(button);
-  }
-
-  public boolean operatorPanelButtonPressed(int button) {
-    return operatorPanel.getRawButtonPressed(button);
-  }
-
-  public boolean operatorPanelButtonReleased(int button) {
-    return operatorPanel.getRawButtonReleased(button);
-  }
-
-  public boolean leftJoystickButtonReleased(int button) {
-    return leftJoystick.getRawButtonReleased(button);
-  }
-
-  public boolean rightJoystickButtonReleased(int button) {
-    return rightJoystick.getRawButtonReleased(button);
+  public boolean rightJoystickTriggerReleased() {
+    return rightJoystick.getTriggerReleased();
   }
 
   // OPERATOR CONTROLLER
-  public boolean XbuttonPressed() {
-    return operatorController.getXButtonPressed();
+
+  public double controllerLeftX() {
+    return operatorController.getLeftX();
   }
 
-  public boolean YbuttonPressed() {
-    return operatorController.getYButtonPressed();
+  public double controllerLeftY() {
+    return operatorController.getLeftY();
   }
 
-  public boolean BbuttonPressed() {
-    return operatorController.getBButtonPressed();
+  public double controllerRightX() {
+    return operatorController.getRightX();
   }
 
-  public boolean AbuttonPressed() {
+  public double controllerRightY() {
+    return operatorController.getRightY();
+  }
+
+  public boolean controllerAButton() {
+    return operatorController.getAButton();
+  }
+
+  public boolean controllerAButtonPressed() {
     return operatorController.getAButtonPressed();
   }
 
-  public boolean controllerLeftTriggerPressed() {
+  public boolean controllerAButtonReleased() {
+    return operatorController.getAButtonReleased();
+  }
+
+  public boolean controllerBButton() {
+    return operatorController.getBButton();
+  }
+
+  public boolean controllerBButtonPressed() {
+    return operatorController.getBButtonPressed();
+  }
+
+  public boolean controllerBButtonReleased() {
+    return operatorController.getBButtonReleased();
+  }
+
+  public boolean controllerXButton() {
+    return operatorController.getXButton();
+  }
+
+  public boolean controllerXButtonPressed() {
+    return operatorController.getXButtonPressed();
+  }
+
+  public boolean controllerXButtonReleased() {
+    return operatorController.getXButtonReleased();
+  }
+
+  public boolean controllerYButton() {
+    return operatorController.getYButton();
+  }
+
+  public boolean controllerYButtonPressed() {
+    return operatorController.getYButtonPressed();
+  }
+
+  public boolean controllerYButtonReleased() {
+    return operatorController.getYButtonReleased();
+  }
+
+  public boolean controllerLeftBumper() {
+    return operatorController.getLeftBumper();
+  }
+
+  public boolean controllerLeftBumperPressed() {
     return operatorController.getLeftBumperPressed();
   }
 
-  public boolean controllerRightTriggerPressed() {
+  public boolean controllerLeftBumperReleased() {
+    return operatorController.getLeftBumperReleased();
+  }
+
+  public boolean controllerRightBumper() {
+    return operatorController.getRightBumper();
+  }
+
+  public boolean controllerRightBumperPressed() {
     return operatorController.getRightBumperPressed();
+  }
+
+  public boolean controllerRightBumperReleased() {
+    return operatorController.getRightBumperReleased();
+  }
+
+  public double controllerLeftTrigger() {
+    return operatorController.getLeftTriggerAxis();
+  }
+
+  public double controllerRightTrigger() {
+    return operatorController.getRightTriggerAxis();
+  }
+
+  public int controllerPOV() {
+    return operatorController.getPOV();
   }
 }
