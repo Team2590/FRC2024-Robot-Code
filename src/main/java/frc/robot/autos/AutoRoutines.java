e1fb63374a964dd8a1cb2d4d60988bcdd9a1f369
asimfazal <fazal.asim@gmail.com>
2024-02-28 17:37:50 -0500

    Added ezAuto

--- src/main/java/frc/robot/autos/AutoRoutines.java
+++ src/main/java/frc/robot/autos/AutoRoutines.java
@@ -3,6 +3,7 @@ package frc.robot.autos;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Superstructure;
 import frc.robot.subsystems.drive.Drive;
+import java.util.function.Function;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class AutoRoutines {
@@ -12,11 +13,13 @@ public class AutoRoutines {
     LoggedDashboardChooser<Command> autoChooser = new LoggedDashboardChooser<>("Auto Choices");
 
     PathPlannerPaths paths = PathPlannerPaths.create();
+    AutoFunction ezAuto = (dynamicPaths) -> buildAuto(paths, drive, superstructure, dynamicPaths);
+
     // Register all the auto routines here
     autoChooser.addOption("OneNoteAuto", oneNoteAuto(paths, drive, superstructure));
+    // This is the same as OneNoteAuto
     autoChooser.addOption(
-        "EasyTwoNoteAuto",
-        buildAuto(paths, drive, superstructure, "shoot", "startB_note1", "snap_shoot", "n2-n3"));
+        "EasyTwoNoteAuto", ezAuto.apply("shoot", "startB_note1", "shoot", "n2-n3", "snap_shoot"));
 
     // dispose of the paths, unused paths with be garbage collected.
     paths.dispose();
@@ -36,23 +39,38 @@ public class AutoRoutines {
   }
 
   /**
-   * Easy way to configure an Auto Routine, just path in the paths.
+   * Easy way to configure an Auto Routine, just pass in the paths.
    *
-   * <p>Use 'shoot' for just shooting or 'snap_shoot' for snaping to target and shooting.
+   * <p>Use 'shoot' for just shooting or 'snap_shoot' for snaping to target and shooting or 'intake'
+   * for running intake.
    */
   private static Command buildAuto(
       PathPlannerPaths pathPlans, Drive drive, Superstructure superstructure, String... paths) {
     AutoCommandBuilder builder = new AutoCommandBuilder(pathPlans, drive, superstructure);
     for (String path : paths) {
-      if ("shoot".equals(path)) {
-        builder.shoot(false);
-      } else if ("snap_shoot".equals(path)) {
-        builder.shoot(true);
-      } else {
-        // if this was to be the first path, AutoCommandBuilder will make it the StartPathCommand.
-        builder.followPath(path);
+      switch (path) {
+        case "shoot":
+          builder.shoot(false);
+          break;
+        case "snap_shoot":
+          builder.shoot(true);
+          break;
+        case "intake":
+          // intake automatically starts up after shoot but just in case if we need it.
+          builder.intake();
+          break;
+        default:
+          // if this was to be the first path, AutoCommandBuilder will make it the StartPathCommand.
+          builder.followPath(path);
+          break;
       }
     }
     return builder.build();
+  }
+
+  @FunctionalInterface
+  interface AutoFunction extends Function<String[], Command> {
+    @Override
+    Command apply(String... args);
   }
 }
