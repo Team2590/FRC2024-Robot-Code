cf37056de630e173ed3dec9484b567d8ab010eb1
aheulitt <94087314+aheulitt@users.noreply.github.com>
2024-03-15 17:56:01 -0400

    New align (#154)
    * new code to address spinning
    
    * fixing command input for climb input
    
    ---------
    
    Co-authored-by: peter-wolfe <wolfe@pobox.com>

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -225,24 +225,23 @@ public class DriveCommands {
         angleSetpoint = 0;
     }
 
-    return Commands.run(
-        (() -> {
-          double theta = drive.getGyroYaw().getDegrees() % 360;
-          double currentError = theta - angleSetpoint;
-          // if (currentError > 180) {
-          //   theta += 360;
-          // } else if (currentError < -180) {
-          //   theta -= 360;
-          // }
-          Logger.recordOutput("Odometry/current theta in stage", theta);
-          drive.runVelocity(
-              new ChassisSpeeds(
-                  // joystick magnitude
-                  xSupplier.getAsDouble() * drive.getMaxLinearSpeedMetersPerSec() * .25,
-                  drive.linearMovementController.calculate(lateralOffset, 0)
-                      * drive.getMaxLinearSpeedMetersPerSec(),
-                  drive.snapController.calculate(theta, angleSetpoint) * .25));
-        }),
-        drive);
+    return Commands.run(() -> {
+      double theta = drive.getGyroYaw().getDegrees() % 360;
+      double currentError = theta - angleSetpoint;
+
+      if (currentError > 180) {
+          currentError -= 360;
+      } else if (currentError < -180) {
+          currentError += 360;
+      }
+
+      Logger.recordOutput("Odometry/current theta in stage", theta);
+
+      drive.runVelocity(new ChassisSpeeds(
+          xSupplier.getAsDouble() * drive.getMaxLinearSpeedMetersPerSec() * .25, // Adjusted linear speed
+          drive.linearMovementController.calculate(lateralOffset, 0) * drive.getMaxLinearSpeedMetersPerSec(), // Lateral movement
+          drive.snapController.calculate(currentError, 0) * .25 // Adjusted angular speed for shortest rotation path
+      ));
+  }, drive);
   }
 }
