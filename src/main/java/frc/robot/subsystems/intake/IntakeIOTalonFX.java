6b8f3f5c4d7afb04f7c108b14c61ed114c71a179
dmanslick <dvvshah@gmail.com>
2024-03-24 15:50:56 -0400

    added canbus to the intake motors

--- src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -6,8 +6,8 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants;
 
 public class IntakeIOTalonFX implements IntakeIO {
-  private TalonFX talon = new TalonFX(Constants.IntakeConstants.INTAKE_ID);
-  private TalonFX follower = new TalonFX(Constants.IntakeConstants.INTAKE_FOLLOWER_ID);
+  private TalonFX talon = new TalonFX(Constants.IntakeConstants.INTAKE_ID, Constants.CANBUS);
+  private TalonFX follower = new TalonFX(Constants.IntakeConstants.INTAKE_FOLLOWER_ID, Constants.CANBUS);
   private final StatusSignal<Double> intakeLeaderPosition = talon.getPosition();
   private final StatusSignal<Double> intakeLeaderVelocity = talon.getVelocity();
   private final StatusSignal<Double> intakeLeaderAppliedVolts = talon.getMotorVoltage();
