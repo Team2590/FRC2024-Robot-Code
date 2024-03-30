7291ae7f8ae0b967a88b7fb22ef61b244aa987c8
Anthony Heulitt <anthonyh1234@gmail.com>
2024-03-30 19:26:13 -0400

    updated setpoints

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -177,9 +177,9 @@ public final class Constants {
     public static final Pose2d BLUE_FLING_POSE =
         new Pose2d(BLUE_ALLIANCE_X, BLUE_ALLIANCE_Y, new Rotation2d(0));
 
-    public static final double[] FLING_DISTANCE = {10.51, 9.09, 7.44, 7.77, 7.82};
-    public static final double[] FLING_ARM_SETPOINT = {0.12, 0.145, 0.168, 0.168, 0.168};
-    public static final double[] FLING_SHOOTER_SETPOINT = {2300, 2300, 2000, 1850, 1850};
+    public static final double[] FLING_DISTANCE = {7.3, 7.726, 8.964, 9.783, 10.2, 10.7};
+    public static final double[] FLING_ARM_SETPOINT = {.14, .12, .12, .12, .11, .11};
+    public static final double[] FLING_SHOOTER_SETPOINT = {1500, 1500, 1750, 1750, 2000, 2000};
   }
 
   public static enum Mode {
