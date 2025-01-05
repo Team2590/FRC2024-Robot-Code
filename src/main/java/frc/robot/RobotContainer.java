33a6559e0560036076d601667b5e11bfeffb0f61
dmanslick <dvvshah@gmail.com>
2025-01-05 16:57:41 -0500

    Created FlyPathBuilder class to create paths on the fly

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,9 +32,12 @@ import frc.robot.subsystems.intake.IntakeIOTalonFX;
 import frc.robot.subsystems.nemesisLED.NemesisLED;
 import frc.robot.subsystems.user_input.UserInput;
 import frc.robot.subsystems.vision.PhotonNoteRunnable;
+import frc.robot.util.FlyPathBuilder;
 import frc.robot.util.PoseEstimator;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+import com.pathplanner.lib.path.PathPlannerPath;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
---
+++ src/main/java/frc/robot/util/FlyPathBuilder.java
@@ -0,0 +1,39 @@
+package frc.robot.util;
+
+import java.util.ArrayList;
+
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class FlyPathBuilder {
+    private ArrayList<Pose2d> positions = new ArrayList<Pose2d>();
+    private GoalEndState endState;
+    private PathConstraints constraints;
+
+    public PathPlannerPath build() {
+        var converted = PathPlannerPath.bezierFromPoses(positions);
+        return new PathPlannerPath(converted, constraints, endState);
+    }
+
+    public FlyPathBuilder addPose(Pose2d pose) {
+        positions.add(pose);
+        return this;
+    }
+
+    public FlyPathBuilder addPoses(Pose2d... poses) {
+        for (var pose : poses) positions.add(pose);
+        return this;
+    }
+
+    public FlyPathBuilder addConstraints(PathConstraints constraints) {
+        this.constraints = constraints;
+        return this;
+    }
+
+    public FlyPathBuilder addEndState(GoalEndState endState) {
+        this.endState = endState;
+        return this;
+    }
+}
